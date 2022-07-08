f242d094f0f59b60a7151a561bf98672590c5775
RosaMoran <rosa.moran.delgado@gmail.com>
2022-07-08 10:42:33 +0200

    deploying

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://RosaMoran.github.io/shop-project/
 export default defineConfig({
   plugins: [react()]
 })
