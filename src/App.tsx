9d11ab35db334eb6f49ca61dcc71db2ce58ea8b4
RosaMoran <rosa.moran.delgado@gmail.com>
2022-07-08 09:15:10 +0200

    only shop

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import {Container } from "react-bootstrap"
-import { Home } from "./pages/Home";
-import { About } from "./pages/About";
+// import { Home } from "./pages/Home"; 
 import { Store } from "./pages/Store";
 import { Navbar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
@@ -12,10 +11,8 @@ function App() {
   <ShoppingCartProvider><Navbar /> 
   <Container className="mb-4">
      <Routes>
-
-       <Route path="/" element={<Home/>} />
-       <Route path="/about" element={<About />} />
-       <Route path="/store" element={<Store />} />
+       {/* <Route path="/" element={<Home/>} /> */}
+       <Route path="/" element={<Store />} />
        </Routes>
    </Container>
    </ShoppingCartProvider>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,9 +8,8 @@ export const Navbar = () => {
     <NavbarBts sticky="top" className="bg-white shadow-sm mb-3">
         <Container>
             <Nav className="me-auto">
-                <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
-                <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
-                <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
+                {/* <Nav.Link to="/" as={NavLink}>Home</Nav.Link> */}
+                <Nav.Link to="/" as={NavLink}>Store</Nav.Link>
             </Nav>
             {cartQuantity > 0 && (
             <Button onClick={openCart} variant="outline-primary" className="rounded-circle" style={{  position: 'relative', width: '3rem', height: '3rem' }} >
